b474edff22a5e73bd9eaa2c2d82e3f58c87895cb
Shay D. <dardenteysha@gmail.com>
2024-08-21 16:54:52 -0500

    Code updated and SheCodes logo added.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-reportWebVitals();
