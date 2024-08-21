3404f402e1472d854711d9975a67b652fd388659
Shay D. <dardenteysha@gmail.com>
2024-08-21 18:14:02 -0500

    Code and files added.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import logo from "./logo.png";
-import "./App.css";
 import Dictionary from "./Dictionary";
+import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+            <img src={logo} className="logo" alt="SheCodes Logo" />
+          </a>
         </header>
-        <main>
-          <Dictionary />
-        </main>
-        <footer className="App-footer">
-          <small>Coded by Teysha Darden</small>
+        <Dictionary defaultKeyword="sunset" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://x.com/Teysha_D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Teysha Darden
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/teysha-d/react-dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-shecodes-dictionary-app.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
   );
 }
+
+export default App;
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,30 +1,71 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Result from "./Result";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
+  const [loaded, setLoaded] = useState(false);
+  const [definition, setDefinition] = useState(null);
+  const [photos, setPhotos] = useState([]);
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  function handleImages(response) {
+    setPhotos(response.data.photos);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function handleResponse(response) {
+    setDefinition(response.data);
+    let apiKey = "eac360db5fc86ft86450f3693e73o43f";
+    let apiUrl = `https://api.shecodes.io/images/v1/search?query=${response.data.word}&key=${apiKey}`;
+    axios
+      .get(apiUrl, { headers: { Authorization: `Bearer ${apiKey}` } })
+      .then(handleImages);
+  }
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  function search() {
+    let apiKey = "eac360db5fc86ft86450f3693e73o43f";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <label>What word do you want to look up?</label>
+            <input
+              type="search"
+              placeholder="Search for a word"
+              defaultValue={props.defaultKeyword}
+              autoFocus={true}
+              className="form-control search-input"
+              onChange={handleKeywordChange}
+            />
+          </form>
+          <small className="hint">i.e. paris, wine, yoga, coding</small>
+        </section>
+        <Result definition={definition} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading!";
+  }
 }
